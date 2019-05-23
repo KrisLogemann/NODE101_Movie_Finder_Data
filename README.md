0ba49b3678f575fc2d5d61dfd653ef0e1e3360e7
Kris Logemann <krisrlogemann@gmail.com>
2019-05-23 14:39:25 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my third project in the Node 101 Module at the San Diego Code School. Here we implement more Express, Morgan and Axios NPM packages into our project.
+
+Thanks for checking out my code on GitHub.
+
+Kris Logemann
--- server/app.js
+++ server/app.js
@@ -1,8 +1,47 @@
 const express = require('express');
-
-
 const app = express();
+const axios = require('axios');
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+var cache = {};
 
-module.exports = app;
+app.get('/', (req, res) => {
+
+  const movieId = req.query.i;
+  const movieTitle = encodeURIComponent(req.query.t);
+
+  if (movieId) {
+    if (cache.hasOwnProperty(movieId)) {
+      res.json(cache[movieId]);
+      console.log('MovieId was sent from the cache.');
+    } else { 
+      axios
+        .get(`http://www.omdbapi.com/?i=${movieId}&apikey=8730e0e`)
+        .then(response => {
+          cache[movieId] = response.data;
+          res.json(cache[movieId]);
+        })
+        .catch(error => {
+          console.log('error', error);
+        });
+    }
+  } else if (movieTitle) {
+    if (cache.hasOwnProperty(movieTitle)) {
+      res.json(cache[movieTitle]);
+    } else {
+      axios
+        .get(`http://www.omdbapi.com/?t=${movieTitle}&apikey=8730e0e`)
+        .then(response => {
+          cache[movieTitle] = response.data;
+          res.json(cache[movieTitle]);
+        })
+        .catch(error => {
+          console.log('error', error);
+        });
+    }
+  }
+
+});
+//export the express application
+module.exports = app;            
